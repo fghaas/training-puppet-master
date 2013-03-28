9972854e6699607efd72f1be8a17c62705678164
Noah Mehl <noah@tritonlimited.com>
2013-03-28 00:41:12 -0400

    Add proxy to provision.sh

--- vagrant/ceph/eric/provision.sh
+++ vagrant/ceph/eric/provision.sh
@@ -7,4 +7,8 @@ echo "192.168.122.114 daisy" >> /etc/hosts
 echo "192.168.122.115 eric" >> /etc/hosts
 echo "192.168.122.116 frank" >> /etc/hosts
 
+echo "http_proxy=\"http://training-puppet-master:3128/\"" >> /etc/environment
+echo "ftp_proxy=\"http://training-puppet-master:3128/\"" >> /etc/environment
+echo "https_proxy=\"http://training-puppet-master:3128/\"" >> /etc/environment
+
 cp -a /vagrant/puppet /etc/
